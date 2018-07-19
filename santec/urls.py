828cfba914408b6183be58b01860dc4253e5cf72
Ermano N. Joseph <jnermano86@gmail.com>
2018-07-19 05:34:25 -0400

    update urls

--- santec/urls.py
+++ santec/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from . import views
 urlpatterns = [
-	url(r'^/$', views.index, name='index'),
+	url(r'^dashboard/$', views.index, name='index'),
 	url(r'^appointment/', views.appointment),
 	url(r'^login/$', views._login, name='login'),
 	url(r'^logout/$', views._logout, name='logout'),
