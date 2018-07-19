195f7e6ff6ad393354dd363bdbb43d3a7b80aa39
Ermano N. Joseph <jnermano86@gmail.com>
2018-07-19 03:38:06 -0400

    editer dossier

--- santec/urls.py
+++ santec/urls.py
@@ -3,10 +3,13 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name ='index'),
 	url(r'^appointment/', views.appointment),
+	url(r'^public/$', views.grand_public, name='public'),
 	url(r'^login/$', views._login, name='login'),
 	url(r'^logout/$', views._logout, name='logout'),
 	url(r'^appointment/', views.appointment, name="appointment"),
 
-	url(r'^ajouter_dossier', views.ajouter_dossier, name="ajouter_dossier"),
-	url(r'^liste_dossiers', views.liste_dossiers, name="liste_dossiers"),
+	url('ajouter_dossier/', views.ajouter_dossier, name="ajouter_dossier"),
+	url('liste_dossiers/', views.liste_dossiers, name="liste_dossiers"),
+	url('editer_dossier/(?P<num>[0-9]+)/$', views.editer_dossier, name="editer_dossier"),
+
 ]
--- santec/views.py
+++ santec/views.py
@@ -1,5 +1,5 @@
 from django.http import *
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from .models import *
 from django.db import transaction
@@ -9,6 +9,10 @@ import sys
 # Create your views here.
 def appointment(request):
 	return render(request, "appointment.html", {'nom': test})
+
+
+def grand_public(request):
+	return render(request, "admin/layout.html")
 
 
 # Create your views here.
@@ -46,7 +50,7 @@ def ajouter_dossier(request):
 	if not request.user.is_authenticated():
 		return HttpResponseRedirect("/login/")
 
-	data = {'heading': 'Nouveau dossier'}
+	data = {'heading': 'Nouveau dossier', "form_action": "/ajouter_dossier/"}
 
 	if request.method == "POST":
 		try:
@@ -113,3 +117,78 @@ def liste_dossiers(request):
 
 	data = {'heading': 'Liste des dossiers', "dossiers": Dossier.objects.all()}
 	return render(request, "admin/liste_dossiers.html", data)
+
+
+"""
+Je sais! c completement con de le faire comme ca, mais il fallait que j'aille dormir.
+"""
+
+
+def editer_dossier(request, num=0):
+	if not request.user.is_authenticated():
+		return HttpResponseRedirect("/login/")
+
+	data = {'heading': 'Nouveau dossier'}
+	dossier = get_object_or_404(Dossier, pk=num)
+
+	data["form_action"] = "/editer_dossier/" + str(dossier.pk) + '/'
+
+	if request.method == "POST":
+		try:
+
+			prenom = request.POST["prenom"]
+			nom = request.POST["nom"]
+			sexe = request.POST["sexe"]
+			age = request.POST["age"]
+
+			departement = request.POST["departement"]
+			ville = request.POST["ville"]
+			rue = request.POST["adresse"]
+			tel = request.POST["tel"]
+
+			profession = request.POST["profession"]
+			religion = request.POST["religion"]
+			groupe_sanguin = request.POST["gsanguin"]
+			allergie = request.POST["allergie"]
+
+			diagnostique = request.POST["diagnostic"]
+
+			address = dossier.id_patient.id_personne.id_adresse
+			address.departement = departement
+			address.ville = ville
+			address.rue = rue
+			address.save()
+
+			personne = dossier.id_patient.id_personne
+			personne.nom = nom
+			personne.prenom = prenom
+			personne.telephone = tel
+			personne.id_adresse = address
+			personne.save()
+
+			patient = dossier.id_patient
+			patient.id_personne = personne
+			patient.age = age
+			patient.sexe = sexe
+			patient.profession = profession
+			patient.religion = religion
+			patient.groupe_sanguin = groupe_sanguin
+			patient.allergie_connu = allergie
+			patient.save()
+
+			dossier.diagnostique = diagnostique
+			dossier.id_patient = patient
+			dossier.save()
+
+		except Exception as ex:
+			transaction.rollback()
+			data['error'] = ex
+			return render(request, "admin/ajouter_dossier.html", data)
+
+		else:
+			transaction.commit()
+			return HttpResponseRedirect("/liste_dossiers/")
+
+	data["dossier"] = dossier
+
+	return render(request, "admin/ajouter_dossier.html", data)
